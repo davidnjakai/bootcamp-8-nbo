a25c78fa001c32b8b364a722f02071440c24e29f
David Njakai <dvdseroney@gmail.com>
2016-06-14 16:56:30 +0300

    add validation to the methods..(method search() still pending)

--- notes.py
+++ notes.py
@@ -5,15 +5,25 @@ class NotesApplication(object):
 	def create(self, note_content):
 		self.notes.append(note_content)
 	def list(self):
-		for i in self.notes:
-			id=0
-			print("Note ID: ",id)
-			id+=1
-			print("By Author ",self.author)
+		if self.notes != [""]:
+			for i in self.notes:
+				id=0
+				print("Note ID: ",id)
+				id+=1
+				print("By Author ",self.author)
+		else:
+			print("Sorry, there are no notes to display")
 	def get(self, note_id):
-		return self.notes[note_id]
+		if note_id < len(self.notes) and note_id >= 0:
+			return self.notes[note_id]
+		else:
+			print("Sorry, no such id")
 	def search(self, search_text):
 		pass
 	def edit(self, note_id, new_content):
-		self.notes[note_id]=new_content
+		if note_id < len(self.notes) and note_id >= 0:	
+			self.notes[note_id]=new_content
+		else:
+			print("sorry, the note you're trying to edit does not exist")
+
 
